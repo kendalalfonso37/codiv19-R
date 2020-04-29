771aa58b3f81c071d6f57f1399e14a28bed01cd4
Kendal Sosa <kendal.alfonso37@gmail.com>
2020-04-29 13:30:29 -0600

    Actualizacion de Datos a 29-abr-20

--- graphs.R
+++ graphs.R
@@ -3,6 +3,7 @@ df = read.csv("my_data.csv")
 df$timestamp = as.Date(strptime(df$timestamp, "%Y-%m-%dT%H:%M:%S"))
 
 plot(x = df$timestamp, y = df$estimated, type = "n", pch = 5, main = "ESA COVID19 Casos estimados vs Casos activos", xlab = "Días", ylab = "Personas", col="red", ylim = c(1,40000))
+plot(x = df$timestamp, y = df$estimated, type = "n", pch = 5, main = "ESA COVID19 Casos estimados vs Casos activos", xlab = "Días", ylab = "Personas", col="red", ylim = c(1,40000))
 lines(df$timestamp,df$estimated, type = "b", col = "red", pch = 5)
 lines(df$timestamp,df$estimated, type = "l", col = "red", pch = 5)
 lines(df$timestamp,df$current, type = "l", col = "blue", pch = 5)
