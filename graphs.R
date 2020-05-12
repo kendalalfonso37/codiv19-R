83eba7ce66bd696c6585db33eb5d8467f75fa464
Kendal Sosa <kendal.alfonso37@gmail.com>
2020-05-11 20:15:55 -0600

    Actualizacion de Datos a Mayo-2020; Agregados Decesos y Recuperados

--- graphs.R
+++ graphs.R
@@ -1,11 +1,28 @@
-#setwd("/home/kendal/Projects/covid19-R/")
+setwd("/home/kendal/Projects/covid19-R/")
 df = read.csv("my_data.csv")
-df$timestamp = as.Date(strptime(df$timestamp, "%Y-%m-%dT%H:%M:%S"))
+df$date = as.Date(df$date)
 
-plot(x = df$timestamp, y = df$estimated, type = "n", pch = 5, main = "ESA COVID19 Casos estimados vs Casos activos", xlab = "Días", ylab = "Personas", col="red", ylim = c(1,40000))
-plot(x = df$timestamp, y = df$estimated, type = "n", pch = 5, main = "ESA COVID19 Casos estimados vs Casos activos", xlab = "Días", ylab = "Personas", col="red", ylim = c(1,40000))
-lines(df$timestamp,df$estimated, type = "b", col = "red", pch = 5)
-lines(df$timestamp,df$estimated, type = "l", col = "red", pch = 5)
-lines(df$timestamp,df$current, type = "l", col = "blue", pch = 5)
-lines(df$timestamp,df$current, type = "b", col = "blue", pch = 5)
+# Estadisticas de Confirmados, Recuperados y Muertes
 
+plot(x = df$date, y = df$cumulative_daily, type = "n", pch = 5, main = "Covid-19 El Salvador Actualizado", xlab = "Días", ylab = "Personas", ylim = c(1,800))
+
+lines(df$date,df$cumulative_daily, type = "l", col = "yellow", pch = 5)
+text(df$date,df$cumulative_daily, df$cumulative_daily, cex = 0.6)
+lines(df$date,df$cumulative_death, type = "l", col = "red", pch = 5)
+text(df$date,df$cumulative_death, df$cumulative_death, cex = 0.6)
+lines(df$date,df$cumulative_recovered, type = "l", col = "blue", pch = 5)
+text(df$date,df$cumulative_recovered, df$cumulative_recovered, cex = 0.6)
+
+legend(x = 'left', y = 'center', legend = c("Confirmados", "Recuperados", "Muertes"), col = c("yellow", "blue", "red"), lty = 1, cex = 0.8)
+
+# Estimacion SIR
+plot(x = df$date, y = df$susceptible, type = "n", pch = 5, main = "Covid-19 El Salvador Modelo SIR", xlab = "Días", ylab = "S,I,R", ylim = c(1,1000))
+
+lines(df$date,df$susceptible, type = "l", col = "yellow", pch = 5)
+text(df$date,df$susceptible, df$susceptible, cex = 0.6)
+lines(df$date,df$cumulative_daily, type = "l", col = "red", pch = 5)
+text(df$date,df$cumulative_daily, df$cumulative_daily, cex = 0.6)
+lines(df$date,df$cumulative_recovered, type = "l", col = "blue", pch = 5)
+text(df$date,df$cumulative_recovered, df$cumulative_recovered, cex = 0.6)
+
+legend(x = 'left', y = 'center', legend = c("S", "R", "I"), col = c("yellow", "blue", "red"), lty = 1, cex = 0.8)
